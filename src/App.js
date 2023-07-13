9c1702dffcc0dd15f0bd5278513f03e14ea340ef
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-07-13 18:16:28 +0200

    Make changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-// import './App.css';
-// import Calculator from './components/Calculator';
-// import QuoteComponent from './QuoteComponent/quote';
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <h1>Math-magicians</h1>
-//       <Calculator />
-//       <QuoteComponent />
-//     </div>
-//   );
-// }
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Navigation from './home/navigation';
+
 function App() {
   return (
     <Router>
@@ -24,4 +12,5 @@ function App() {
     </Router>
   );
 }
+
 export default App;
